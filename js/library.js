3cd6af046de9cfdacfea7c4a8cbf823d5efe1866
ilhamgurbanly10 <Ilham_gurbanly@mail.ru>
2022-08-15 13:25:36 +0500

    last-commit

--- js/library.js
+++ js/library.js
@@ -9,7 +9,46 @@ flashForm();
 
 counter();
 
+flashChangeContainers();
+
 // the-end-of-calling-functions
+
+
+
+// flash-change-containers
+
+function flashChangeContainers() {
+
+  const changeContainers = (el) => {
+
+    const buttons = el.querySelectorAll('.fl-change-containers-btn');
+    const items = el.querySelectorAll('.fl-change-containers-item');
+
+    // functions
+    function show() {
+      const index = this.getAttribute('data-index');
+      for (let i = 0; i < items.length; i++) { i == index ? active(i) : deactive(i); }
+    }
+
+    const active = (i) => { items[i].classList.add('fl-show'); buttons[i].classList.add('fl-active'); }
+
+    const deactive = (i) => { items[i].classList.remove('fl-show'); buttons[i].classList.remove('fl-active'); }
+
+    // adding-functions
+    for (let i = 0; i < buttons.length; i++) { 
+      buttons[i].addEventListener('click', show);
+      buttons[i].setAttribute('data-index', i); 
+    }
+
+  }
+
+  // adding-function
+  const elements = document.querySelectorAll('.fl-change-containers');
+  for (let i = 0; i < elements.length; i++) { changeContainers(elements[i]); }
+
+}
+
+// the-end-of-flash-change-containers
 
 
 
